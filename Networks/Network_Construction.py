5e436aa260ef1d4ce3c2f3632833d9731d64cd3c
txm0324 <tm715705@gmail.com>
2025-06-18 09:59:40 +0200

    Add Threshold for Interactions

--- Networks/Network_Construction.py
+++ Networks/Network_Construction.py
@@ -32,16 +32,24 @@ dgi_df = pd.read_csv("./data/DGI_Final.csv", index_col=0)
 print("- Import PPI-Template (String Database)")
 # columns: source, target
 # Code from 'Networks.ipynb' Point 3: Protein-Protein-Network
-# ppi_df = pd.read_csv("./data/final_PPI_String.csv", index_col=0)
-ppi_df = pd.read_csv("/sybig/home/tmu/Schreibtisch/Thesis/Networks/results/PPI/final_PPI_String.csv", index_col=0)
+ppi_df_STRING = pd.read_csv("./data/final_PPI_String.csv")
+ppi_df_IntAct = pd.read_csv("./data/final_ppi_intAct.csv")
+
+ppi_df = pd.concat([ppi_df_STRING, ppi_df_IntAct], ignore_index=True)
+ppi_df.drop_duplicates(subset=['source', 'target'], inplace=True)
+
+# ppi_df = pd.read_csv("/sybig/home/tmu/Schreibtisch/Thesis/Networks/results/PPI/final_PPI_String.csv", index_col=0)
+
+# Threshold Test
+ppi_df = ppi_df[ppi_df['combined_score'] > 500]
 
 # ------------------
 # Step 1: Preprocess 
 # ------------------
 
 # cell-line and drug sample dataset of TUGDA (GDSC)
-# gdsc_dataset = pd.read_csv('/Users/tm03/Desktop/TUGDA_1/data/GDSCDA_fpkm_AUC_all_drugs.zip', index_col=0)
-gdsc_dataset = pd.read_csv('/sybig/home/tmu/TUGDA/data/GDSCDA_fpkm_AUC_all_drugs.zip', index_col=0)
+gdsc_dataset = pd.read_csv('/Users/tm03/Desktop/TUGDA_1/data/GDSCDA_fpkm_AUC_all_drugs.zip', index_col=0)
+# gdsc_dataset = pd.read_csv('/sybig/home/tmu/TUGDA/data/GDSCDA_fpkm_AUC_all_drugs.zip', index_col=0)
 gene_list = gdsc_dataset.columns[0:1780]
 drug_list = gdsc_dataset.columns[1780:]
 
@@ -79,8 +87,6 @@ drug_to_direct_targets = (
     .to_dict()
 )
 
-print("- Define Indirect Targets")
-
 # Mapping from Gen <--> PPI Partner (bidrectional)
 ppi_genes = ppi_df[['source', 'target']].values.tolist()
 gene_to_partners = defaultdict(set)
@@ -92,6 +98,8 @@ for source, target in ppi_genes:
 total_direct_targets = sum(len(targets) for targets in drug_to_direct_targets.values())
 print(f"Number of direct targets: {total_direct_targets}") # 23,063
 
+print("- Define Indirect Targets")
+
 # Collect all edges (drug -> direct target -> indirect target)
 all_edges = []
 drugs2target_edges = defaultdict(list)
@@ -100,13 +108,13 @@ for drug, direct_targets in tqdm(drug_to_direct_targets.items(), desc="Processin
     # here just for 176 instead of 200 (TUGDA) because just for 176 Drugs Drug-Gene-Interactions was found
     for direct_target in direct_targets:
         if direct_target not in gene_list:
-            continue                      # nur Gene aus TUGDA zulassen
+            continue # Just genes in TUGDA sample
         # Level 1: direct edge: Drug -> Target 
         all_edges.append((drug, direct_target, drug)) # second drug to indicate which drug this interaction belongs to
 
         # indirect targets via PPI 
         indirect_targets = gene_to_partners.get(direct_target, set())
-        for partner in list(indirect_targets)[:10]: # Threshold, Downstream
+        for partner in list(indirect_targets): # Threshold, Downstream
             if partner in gene_list:
             # Level 2: indirect edge: Target -> Partner 
                 all_edges.append((direct_target, partner, drug)) # drug to indicate which drug this interaction belongs to
@@ -114,187 +122,187 @@ for drug, direct_targets in tqdm(drug_to_direct_targets.items(), desc="Processin
 lvl1_edges_df = pd.DataFrame(all_edges, columns = ["source", "target", "drug"]).drop_duplicates()
 print(f"- All direct/indirect gene pairs: {lvl1_edges_df.shape[0]:,}") # 31,717,030 without classification, 253,693 with classfication
 
-print("Define Connections")
+# print("Define Connections")
 
-# only keep edges that also occur in the PPI 
-# Create a list of all valid PPI edges as a tuple 
+# # only keep edges that also occur in the PPI 
+# # Create a list of all valid PPI edges as a tuple 
 
-ppi_set = set()
-# is always saved in the same format
-# For each pair (source, target) you ensure that it is always saved in the same format: always as (smaller_gen, larger_gen), alphabetical
-for source, target in ppi_df[['source', 'target']].values:
-    if source <= target:
-        ppi_set.add((source, target))
-    else:
-        ppi_set.add((target, source))
-
-# Filter
-# e.g. tuple(sorted(['TP53', 'BRCA1'])) → ('BRCA1', 'TP53'), just same them one time instead of each time (a,b)/(b,a) is included 
-sources = lvl1_edges_df['source'].values
-targets = lvl1_edges_df['target'].values
-
-mask = [tuple(sorted([source, target])) in ppi_set for source, target in zip(sources, targets)]
-lvl1_edges_df = lvl1_edges_df[mask].reset_index(drop=True)
-
-print(f"- Edges after PPI filtering: {lvl1_edges_df.shape[0]:,}")
-print("Final Dataframe 'lv1_edges_df'")
-print(lvl1_edges_df.head())
-
-print("Level 2: Pathways")
-
-all_lvl2_edges = []
-
-# get pathways by library gseapy
-# here I used KEGG_2021_Human, yon can also use 'GO_Biological_Process_2023', 'GO_Cellular_Component_2023' 'GO_Molecular_Function_2023', 'KEGG_2021_Human', 'Reactome_2022'
-# gp.get_library_name()
-kegg_gmt = gp.parser.get_library('KEGG_2021_Human', organism='Human', min_size=3, max_size=2000)
-
-print("- Number of Pathways:", len(kegg_gmt))
-
-# Number of genes 
-kegg_genes = set(gene for genes in kegg_gmt.values() for gene in genes)
-network_genes = set(lvl1_edges_df[["source", "target"]].values.ravel())
-print("- Network genes:", len(network_genes))
-filtered_kegg_genes = network_genes.intersection(kegg_genes)
-print("- Filtered Genes (in network and KEGG):", len(filtered_kegg_genes))
-
-# Create DataFrame with (Pathway, Gene), only for genes in the network
-kegg_df_data = []
-for term, genes in kegg_gmt.items():
-    for gene in genes:
-        if gene in filtered_kegg_genes:
-            kegg_df_data.append((term, gene))
-
-for drug in drug_list:   
-    # Create DataFrame with (Pathway, Gene) for each drug, only for genes in the network
-    kegg_df_data = []
-    for term, genes in kegg_gmt.items():
-        for gene in genes:
-            if gene in filtered_kegg_genes:
-                kegg_df_data.append((term, gene))
-
-    # Add drug column to indicate which drug this interaction belongs to
-    kegg_df = pd.DataFrame(kegg_df_data, columns=["source", "target"]).drop_duplicates()
-    kegg_df['drug'] = drug
-    
-    # Speichern
-    all_lvl2_edges.append(kegg_df)
-
-# Merge Pathway Dataframes
-lvl2_edges_df = pd.concat(all_lvl2_edges, axis=0).reset_index(drop=True)
-print("Final Dataframe 'lv2_edges_df'")
-print(lvl2_edges_df.head())
-
-print(f"- Pathway edges added: {kegg_df.shape[0]:,}")
-final_network = pd.concat([
-    lvl1_edges_df,
-    lvl2_edges_df,
-], axis=0).drop_duplicates().reset_index(drop=True)
-
-print(f"- Final network contains {final_network.shape[0]:,} unique edges.")
-print(final_network.head())
-
-# ------------------------------------------
-# Step 3: Create binary matrix for each drug
-# ------------------------------------------
-
-# Directory to save the matrices
-output_dir = "./results/Network"
-npz_output_dir = os.path.join(output_dir, "drug_sparse_matrices_npz")
-os.makedirs(npz_output_dir, exist_ok=True)
-
-print("\n" + "="*31)
-print("Step 3: Create adjacency matrix")
-print("="*31)
-
-# For each drug in drug_list
-for drug in tqdm(drug_list, desc="Drug Processing"):  
-
-    # Define involving edges and nodes of the current drug
-    drug_mask = final_network['drug'] == drug
-    drug_edges = final_network[drug_mask]
-    nodes = pd.concat([drug_edges['source'], drug_edges['target']]).unique()
-    
-    # Mapping Gene -> Index for quick access 
-    node_index = {node: idx for idx, node in enumerate(nodes)}
-    dimension = len(nodes)
-
-    # Initialize an empty binary adjacency matrix
-    binary_matrix = np.zeros((dimension, dimension))
-
-    # add self-loops (node with itself is a interaction - diagonal elements)
-    np.fill_diagonal(binary_matrix, 1)
-
-    # Create for each drug its binary matrix
-    for _, row in drug_edges.iterrows(): 
-        i = node_index[row["source"]]
-        j = node_index[row["target"]]
-        binary_matrix[i, j] = 1
-        binary_matrix[j, i] = 1 # Symmetric update for undirected graph 
-    
-    # Save matrix as Sparse Matrix Representations (Storage-efficient, Charge-optimized, ML-compatible)
-    '''
-    Input : 
-    10  20  0  0  0  0
-    0  30  0  4  0  0
-    0   0 50 60 70  0
-    0   0  0  0  0 80
-
-    Output :  
-    A = [10 20 30 4 50 60 70 80], # vector of the non-zero elements 
-    IA = [0 2 4 7 8] #  IA[0] = 0. IA[1] = IA[0] + no of non-zero elements in row 0
-    JA = [0 1 1 3 2 3 4 5] # column indices of elements in A
-    '''
-    sparse_matrix = csr_matrix(binary_matrix)
-
-    # Save complete data including metadata (gene names) for analysis and interpretation
-    np.savez(
-        os.path.join(npz_output_dir, f"{drug}_full.npz"),
-        data=sparse_matrix.data, # non-zero values
-        indices=sparse_matrix.indices, # column indices of these values
-        indptr=sparse_matrix.indptr, # pointer to when a new row begins
-        shape=sparse_matrix.shape, # dimensions of the matrix
-        genes_pathways=nodes  # List of genes and pathways that occur in the matrix
-    )
-
-
-# --------------------------------------
-# Step 4: Graph-Representation (Example)
-# --------------------------------------
-
-# import networkx as nx
-# import matplotlib.pyplot as plt
-# import pandas as pd
-
-# print("Step 4: Graph (First Drug): First 100 Edges")
-
-# example_drug = drug_list[0]
-# drug_network = final_network[final_network['drug'] == example_drug]
-
-# G = nx.Graph()
-
-# # Füge Kanten hinzu
-# for _, row in drug_network.head(100).iterrows():
-#     source = row['source']
-#     target = row['target']
-#     G.add_edge(source, target)
-
-# pos = nx.spring_layout(G, k=0.5, seed=42)
-
-# node_colors = []
-# for node in G.nodes:
-#     if node in common_genes:
-#         node_colors.append('lightblue') # Gene
-#     elif node in kegg_gmt.keys():
-#         node_colors.append('lightgreen') # Pathways
+# ppi_set = set()
+# # is always saved in the same format
+# # For each pair (source, target) you ensure that it is always saved in the same format: always as (smaller_gen, larger_gen), alphabetical
+# for source, target in ppi_df[['source', 'target']].values:
+#     if source <= target:
+#         ppi_set.add((source, target))
 #     else:
-#         node_colors.append('gray') # Drug
+#         ppi_set.add((target, source))
 
-# # Plot
-# plt.figure(figsize=(12, 8))
-# nx.draw(G, pos, with_labels=True, node_size=300, node_color=node_colors,
-#         font_size=10, edge_color='gray', alpha=0.8, width=1.2)
-# plt.title(f"Drug-Specific Network for '{example_drug}'")
-# plt.axis("off")
-# plt.tight_layout()
-# plt.show()
+# # Filter
+# # e.g. tuple(sorted(['TP53', 'BRCA1'])) → ('BRCA1', 'TP53'), just same them one time instead of each time (a,b)/(b,a) is included 
+# sources = lvl1_edges_df['source'].values
+# targets = lvl1_edges_df['target'].values
+
+# mask = [tuple(sorted([source, target])) in ppi_set for source, target in zip(sources, targets)]
+# lvl1_edges_df = lvl1_edges_df[mask].reset_index(drop=True)
+
+# print(f"- Edges after PPI filtering: {lvl1_edges_df.shape[0]:,}")
+# print("Final Dataframe 'lv1_edges_df'")
+# print(lvl1_edges_df.head())
+
+# print("Level 2: Pathways")
+
+# all_lvl2_edges = []
+
+# # get pathways by library gseapy
+# # here I used KEGG_2021_Human, yon can also use 'GO_Biological_Process_2023', 'GO_Cellular_Component_2023' 'GO_Molecular_Function_2023', 'KEGG_2021_Human', 'Reactome_2022'
+# # gp.get_library_name()
+# kegg_gmt = gp.parser.get_library('KEGG_2021_Human', organism='Human', min_size=3, max_size=2000)
+
+# print("- Number of Pathways:", len(kegg_gmt))
+
+# # Number of genes 
+# kegg_genes = set(gene for genes in kegg_gmt.values() for gene in genes)
+# network_genes = set(lvl1_edges_df[["source", "target"]].values.ravel())
+# print("- Network genes:", len(network_genes))
+# filtered_kegg_genes = network_genes.intersection(kegg_genes)
+# print("- Filtered Genes (in network and KEGG):", len(filtered_kegg_genes))
+
+# # Create DataFrame with (Pathway, Gene), only for genes in the network
+# kegg_df_data = []
+# for term, genes in kegg_gmt.items():
+#     for gene in genes:
+#         if gene in filtered_kegg_genes:
+#             kegg_df_data.append((term, gene))
+
+# for drug in drug_list:   
+#     # Create DataFrame with (Pathway, Gene) for each drug, only for genes in the network
+#     kegg_df_data = []
+#     for term, genes in kegg_gmt.items():
+#         for gene in genes:
+#             if gene in filtered_kegg_genes:
+#                 kegg_df_data.append((term, gene))
+
+#     # Add drug column to indicate which drug this interaction belongs to
+#     kegg_df = pd.DataFrame(kegg_df_data, columns=["source", "target"]).drop_duplicates()
+#     kegg_df['drug'] = drug
+    
+#     # Speichern
+#     all_lvl2_edges.append(kegg_df)
+
+# # Merge Pathway Dataframes
+# lvl2_edges_df = pd.concat(all_lvl2_edges, axis=0).reset_index(drop=True)
+# print("Final Dataframe 'lv2_edges_df'")
+# print(lvl2_edges_df.head())
+
+# print(f"- Pathway edges added: {kegg_df.shape[0]:,}")
+# final_network = pd.concat([
+#     lvl1_edges_df,
+#     lvl2_edges_df,
+# ], axis=0).drop_duplicates().reset_index(drop=True)
+
+# print(f"- Final network contains {final_network.shape[0]:,} unique edges.")
+# print(final_network.head())
+
+# # ------------------------------------------
+# # Step 3: Create binary matrix for each drug
+# # ------------------------------------------
+
+# # Directory to save the matrices
+# output_dir = "./results/Network"
+# npz_output_dir = os.path.join(output_dir, "drug_sparse_matrices_npz")
+# os.makedirs(npz_output_dir, exist_ok=True)
+
+# print("\n" + "="*31)
+# print("Step 3: Create adjacency matrix")
+# print("="*31)
+
+# # For each drug in drug_list
+# for drug in tqdm(drug_list, desc="Drug Processing"):  
+
+#     # Define involving edges and nodes of the current drug
+#     drug_mask = final_network['drug'] == drug
+#     drug_edges = final_network[drug_mask]
+#     nodes = pd.concat([drug_edges['source'], drug_edges['target']]).unique()
+    
+#     # Mapping Gene -> Index for quick access 
+#     node_index = {node: idx for idx, node in enumerate(nodes)}
+#     dimension = len(nodes)
+
+#     # Initialize an empty binary adjacency matrix
+#     binary_matrix = np.zeros((dimension, dimension))
+
+#     # add self-loops (node with itself is a interaction - diagonal elements)
+#     np.fill_diagonal(binary_matrix, 1)
+
+#     # Create for each drug its binary matrix
+#     for _, row in drug_edges.iterrows(): 
+#         i = node_index[row["source"]]
+#         j = node_index[row["target"]]
+#         binary_matrix[i, j] = 1
+#         binary_matrix[j, i] = 1 # Symmetric update for undirected graph 
+    
+#     # Save matrix as Sparse Matrix Representations (Storage-efficient, Charge-optimized, ML-compatible)
+#     '''
+#     Input : 
+#     10  20  0  0  0  0
+#     0  30  0  4  0  0
+#     0   0 50 60 70  0
+#     0   0  0  0  0 80
+
+#     Output :  
+#     A = [10 20 30 4 50 60 70 80], # vector of the non-zero elements 
+#     IA = [0 2 4 7 8] #  IA[0] = 0. IA[1] = IA[0] + no of non-zero elements in row 0
+#     JA = [0 1 1 3 2 3 4 5] # column indices of elements in A
+#     '''
+#     sparse_matrix = csr_matrix(binary_matrix)
+
+#     # Save complete data including metadata (gene names) for analysis and interpretation
+#     np.savez(
+#         os.path.join(npz_output_dir, f"{drug}_full.npz"),
+#         data=sparse_matrix.data, # non-zero values
+#         indices=sparse_matrix.indices, # column indices of these values
+#         indptr=sparse_matrix.indptr, # pointer to when a new row begins
+#         shape=sparse_matrix.shape, # dimensions of the matrix
+#         genes_pathways=nodes  # List of genes and pathways that occur in the matrix
+#     )
+
+
+# # --------------------------------------
+# # Step 4: Graph-Representation (Example)
+# # --------------------------------------
+
+# # import networkx as nx
+# # import matplotlib.pyplot as plt
+# # import pandas as pd
+
+# # print("Step 4: Graph (First Drug): First 100 Edges")
+
+# # example_drug = drug_list[0]
+# # drug_network = final_network[final_network['drug'] == example_drug]
+
+# # G = nx.Graph()
+
+# # # Füge Kanten hinzu
+# # for _, row in drug_network.head(100).iterrows():
+# #     source = row['source']
+# #     target = row['target']
+# #     G.add_edge(source, target)
+
+# # pos = nx.spring_layout(G, k=0.5, seed=42)
+
+# # node_colors = []
+# # for node in G.nodes:
+# #     if node in common_genes:
+# #         node_colors.append('lightblue') # Gene
+# #     elif node in kegg_gmt.keys():
+# #         node_colors.append('lightgreen') # Pathways
+# #     else:
+# #         node_colors.append('gray') # Drug
+
+# # # Plot
+# # plt.figure(figsize=(12, 8))
+# # nx.draw(G, pos, with_labels=True, node_size=300, node_color=node_colors,
+# #         font_size=10, edge_color='gray', alpha=0.8, width=1.2)
+# # plt.title(f"Drug-Specific Network for '{example_drug}'")
+# # plt.axis("off")
+# # plt.tight_layout()
+# # plt.show()
--- Networks/PPI.py
+++ Networks/PPI.py
@@ -1,39 +1,145 @@
-import pandas as pd
+import pandas as pd # for Dataframe
+import re # for regular expressions (RegEx)
 
 # Create a Dataframe of a Protein-Protein-Network
 
+# -----------------------
+# Step 1: STRING Database 
+# -----------------------
+
+print("\n" + "="*38)
+print("Step 1: Download and Preprocess STRING")
+print("="*38)
+
 # Load STRING Protein-Protein Interaction (PPI) data and mapping file
-ppi = pd.read_csv("raw_data/PPI/9606.protein.links.v12.0.txt.gz", sep=" ")
-gene_name = pd.read_csv("raw_data/PPI/protein2gene.csv", sep='\t')
+ppi_string = pd.read_csv("raw_data/PPI/9606.protein.links.v12.0.txt.gz", sep=" ")
+gene_name_string = pd.read_csv("raw_data/PPI/protein2gene_STRING.csv", sep='\t')
 
 # Remove species prefix "9606." from ENSP IDs in PPI dataset
-ppi["protein1"] = ppi["protein1"].str.replace("9606.", "", regex=False)
-ppi["protein2"] = ppi["protein2"].str.replace("9606.", "", regex=False)
+ppi_string["protein1"] = ppi_string["protein1"].str.replace("9606.", "", regex=False)
+ppi_string["protein2"] = ppi_string["protein2"].str.replace("9606.", "", regex=False)
 
 # Rename columns for clarity
-gene_name = gene_name.rename(columns={"protein_ENSP": "protein", "gene name": "gene"})
+gene_name_string = gene_name_string.rename(columns={"protein_ENSP": "protein", "gene name": "gene"})
 
 # Map protein1 (source) to gene names using the gene_name lookup table
-ppi = ppi.merge(gene_name, how="left", left_on="protein1", right_on="protein")
-ppi = ppi.rename(columns={"gene": "source"})
-ppi = ppi.drop(columns=["protein"])  # Remove redundant column after merge
+ppi_string = ppi_string.merge(gene_name_string, how="left", left_on="protein1", right_on="protein")
+ppi_string = ppi_string.rename(columns={"gene": "source"})
+ppi_string = ppi_string.drop(columns=["protein"])  # Remove redundant column after merge
 
 # Map protein2 (target) to gene names using the same lookup table
-ppi = ppi.merge(gene_name, how="left", left_on="protein2", right_on="protein")
-ppi = ppi.rename(columns={"gene": "target"})
-ppi = ppi.drop(columns=["protein"])  # Remove redundant column after merge
+ppi_string = ppi_string.merge(gene_name_string, how="left", left_on="protein2", right_on="protein")
+ppi_string = ppi_string.rename(columns={"gene": "target"})
+ppi_string = ppi_string.drop(columns=["protein"])  # Remove redundant column after merge
 
 # Keep only relevant columns: source and target gene names
-ppi = ppi[["source", "target"]]
+ppi_string = ppi_string[["source", "target", "combined_score"]]
 
 # Filter out rows where either source or target gene is missing ("Not Found")
-mask = (ppi['source'] != 'Not Found') & (ppi['target'] != 'Not Found')
-final_ppi_string = ppi[mask]
+mask = (ppi_string['source'] != 'Not Found') & (ppi_string['target'] != 'Not Found')
+final_ppi_string = ppi_string[mask]
 
+print("Final Dataframe - STRING:")
+print(final_ppi_string.head(10))
 
-print("Final Dataframe:")
-print(final_ppi_string.head())
+# -----------------------
+# Step 2: IntAct Database 
+# -----------------------
 
+print("\n" + "="*38)
+print("Step 2: Download and Preprocess IntAct")
+print("="*38)
+
+# Functios to preprocess files
+def extract_uniprotkb(text):
+    """
+    Extracts the UniProtKB identifier from a string.
+
+    Parameters:
+    - text: A string containing the UniProtKB ID.
+
+    Returns:
+    - string: The extracted UniProtKB ID or the original text if no ID is found.
+    """
+    match = re.search(r'uniprotkb:([A-Z0-9]+)', text)
+    return match.group(1) if match else text
+
+def extract_intact_miscore(text):
+    """
+    Extracts the Confidence value(s) (MI-Score) from a string.
+
+    Parameters:
+    - text: A string containing the MI-Score.
+
+    Returns:
+    - string: The extracted Confidence value(s) or the original text if no ID is found.
+    """
+    match = re.search(r'intact-miscore:([0-9.]+)', text)
+    return float(match.group(1)) if match else None
+
+def process_df(df):
+    """
+    Processes a DataFrame by extracting relevant information and applying functions to specific columns.
+
+    Parameters:
+    - df: A pandas DataFrame containing columns 'Protein', 'Interactor', 'Confidence value(s)', 'Database'.
+
+    Returns:
+    - pd.DataFrame: A processed DataFrame with additional columns for taxa and names.
+    """
+    df['Protein'] = df['#ID(s) interactor A'].apply(extract_uniprotkb)
+    df['Interactor'] = df['ID(s) interactor B'].apply(extract_uniprotkb)
+    df['intact_miscore'] = df['Confidence value(s)'].apply(extract_intact_miscore)
+    df['Database'] = 'IntAct'
+    return df
+
+# Load STRING Protein-Protein Interaction (PPI) data and mapping file
+try:
+    ppi_intact = pd.read_csv("/Users/tm03/Desktop/Uni/Softwarepraktikum/Softwarepraktikum_local/Encoding/PPI/Aktuell/human.txt", sep="\t")
+except FileNotFoundError:
+    raise FileNotFoundError(
+        f"The file '{"raw_data/PPI/human.txt"}' was not found.\n"
+        "Please download the file from the following website:\n"
+        "https://www.ebi.ac.uk/intact/interactomes\n\n"
+        "→ Choose: *Homo sapiens* as the organism\n"
+        "→ Format: *MITAB 2.7*\n"
+        "Then save the file as 'human.txt' in the folder 'raw_data/PPI/'"
+    )
+
+gene_name_intact = pd.read_csv("raw_data/PPI/protein2gene_IntAct.csv", sep='\t')
+
+# Run preprocess functions
+ppi_intact = process_df(ppi_intact)
+selected_columns = ['Protein', 'Interactor', 'intact_miscore', 'Database']
+ppi_intact = ppi_intact[selected_columns]
+ppi_intact.rename(columns={"intact_miscore" : "combined_score"}, inplace=True)
+
+# Drop rows with missing value
+ppi_intact = ppi_intact[~(ppi_intact[['Protein', 'Interactor']] == '-').any(axis=1)]
+
+# Rename columns for clarity
+gene_name_intact = gene_name_intact.rename(columns={'UniProt_ID': 'Protein_ID', 'gene name': 'Gene_Name'})
+
+# Map protein1 (source) to gene names using the gene_name lookup table
+ppi_intact = ppi_intact.merge(gene_name_intact, left_on='Protein', right_on='Protein_ID', how='left')
+ppi_intact = ppi_intact.rename(columns={'Gene_Name': 'source'})
+ppi_intact = ppi_intact.drop(columns='Protein_ID')
+
+# Map protein2 (target) to gene names using the same lookup table
+ppi_intact = ppi_intact.merge(gene_name_intact, left_on='Interactor', right_on='Protein_ID', how='left')
+ppi_intact = ppi_intact.rename(columns={'Gene_Name': 'target'})
+ppi_intact = ppi_intact.drop(columns='Protein_ID')
+
+# Keep only relevant columns: source, target gene names and the score as threshold
+ppi_intact = ppi_intact[["source", "target", "combined_score"]]
+ppi_intact['combined_score'] = ppi_intact['combined_score'] * 1000
+
+# Filter out rows where either source or target gene is missing ("Not Found")
+mask = (ppi_intact['source'] != 'Not Found') & (ppi_intact['target'] != 'Not Found')
+final_ppi_intact = ppi_intact[mask]
+
+print("Final Dataframe - IntAct:")
+print(final_ppi_intact.head(10))
 """
 Comment on mapping process:
 
@@ -41,7 +147,7 @@ Comment on mapping process:
 
 Steps to generate the mapping file (protein2gene.csv):
 1. Extract all unique ENSP IDs from both 'protein1' and 'protein2' columns: 
-   - Run this code snippet to save the unique IDs:
+   - Run this code snippet to save the unique IDs (STRING):
      ppi = pd.read_csv("raw_data/PPI/9606.protein.links.v12.0.txt.gz", sep=" ")
      ppi["protein1"] = ppi["protein1"].str.replace("9606.", "", regex=False)
      ppi["protein2"] = ppi["protein2"].str.replace("9606.", "", regex=False)
@@ -49,6 +155,13 @@ Steps to generate the mapping file (protein2gene.csv):
      unique_ensp = ensp_series.unique()
      ensp_df = pd.DataFrame(unique_ensp, columns=['ENSP IDs'])
      ensp_df.to_csv('raw_data/PPI/unique_ENSP_IDs.csv', index=False)
+   - Run this code snippet to save the unique IDs (IntAct):
+     unique_uniprot_ids = pd.concat([IntAct_df_cleaned['Protein'], IntAct_df_cleaned['Interactor']]).dropna().unique()
+     unique_uniprot_list = unique_uniprot_ids.tolist()
+     filtered_uniprot_list = [uid for uid in unique_uniprot_list if not uid.startswith('intact:')]
+     filtered_uniprot_ids = np.array(filtered_uniprot_list)
+     unique_uniprot_df = pd.DataFrame({'UniProt_ID': filtered_uniprot_ids})
+     unique_uniprot_df.to_csv('unique_uniprot_ids.csv', index=False)
 
 2. Use idconverter.py to map IDs:
    - Command example: python idconverter.py --input unique_ENSP_IDs.csv --output protein2gene.csv --n 1
@@ -61,10 +174,11 @@ Steps to generate the mapping file (protein2gene.csv):
 
 Notes:
 - During mapping, some ENSP IDs could not be converted to gene names ("Not Found")
-    - For protein1/source: ~2647 interactions were lost due to "Not found"
-    - For protein2/target: ~2647 interactions were lost due to "Not found"
-    - After filtering both ends, ~2647 unique interactions were removed entirely
+    - For protein1/source: 2647 interactions were lost due to "Not found"
+    - For protein2/target: 2647 interactions were lost due to "Not found"
+    - After filtering both ends, 2647 unique interactions were removed entirely
 """
 
 # Optional: Save final cleaned PPI network to CSV (its already uploaded under Networks/data/final_PPI_Sting)
-# final_ppi_string.to_csv("./results/PPI/final_PPI_String.csv", index=False)
+# final_ppi_string.to_csv("./data/final_PPI_String.csv", index=False)
+# final_ppi_string.to_csv("./data/final_PPI_IntAct.csv", index=False)
